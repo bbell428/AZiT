d9366df06044106ccf5ac59c7f2593facc4207fb
Jisoo <voidmule.xx@gmail.com>
2024-11-06 02:06:45 +0900

    [Design]: EmojiView, EmojiUploadView 이모지 키보드 디자인 수정

---
+++ Azit/Azit/Views/Upload/EmojiPickerView.swift
@@ -0,0 +1,99 @@
+//
+//  EmojiPickerView.swift
+//  Azit
+//
+//  Created by 홍지수 on 11/6/24.
+//
+
+import SwiftUI
+import Foundation
+import Smile
+
+public struct EmojiPickerView: View {
+
+    @Environment(\.dismiss)
+    var dismiss
+
+    @Binding
+    public var selectedEmoji: Emoji?
+
+    @State
+    private var search: String = ""
+
+    private var selectedColor: Color
+    private var searchEnabled: Bool
+
+    public init(selectedEmoji: Binding<Emoji?>, searchEnabled: Bool = false, selectedColor: Color = .blue, emojiProvider: EmojiProvider = DefaultEmojiProvider()) {
+        self._selectedEmoji = selectedEmoji
+        self.selectedColor = selectedColor
+        self.searchEnabled = searchEnabled
+        self.emojis = emojiProvider.getAll()
+    }
+
+    let columns = [
+        GridItem(.adaptive(minimum: 45))
+    ]
+
+    let emojis: [Emoji]
+
+    private var searchResults: [Emoji] {
+        if search.isEmpty {
+            return emojis
+        } else {
+            return emojis
+                .filter { $0.name.lowercased().contains(search.lowercased()) }
+        }
+    }
+
+    public var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(searchResults, id: \.self) { emoji in
+                    RoundedRectangle(cornerRadius: 16)
+                        .fill((selectedEmoji == emoji ? selectedColor : Color.subColor2).opacity(0.3))
+                        .frame(width: 40, height: 40)
+                        .overlay {
+                            Text(emoji.value)
+                                .font(.title)
+                        }
+                        .onTapGesture {
+                            selectedEmoji = emoji
+                            dismiss()
+                        }
+                }
+            }
+            .padding(.horizontal)
+        }
+        .frame(maxHeight: .infinity)
+        .searchable(text: $search)
+    }
+
+}
+
+public struct Emoji: Hashable {
+
+    public let value: String
+    public let name: String
+
+    public init(value: String, name: String) {
+        self.value = value
+        self.name = name
+    }
+
+}
+
+public final class DefaultEmojiProvider: EmojiProvider {
+
+    public init() { }
+
+    public func getAll() -> [Emoji] {
+        return Smile.list().map({ Emoji(value: $0, name: name(emoji: $0).first ?? "") })
+    }
+
+}
+
+import Foundation
+
+public protocol EmojiProvider {
+    func getAll() -> [Emoji]
+}
--- Azit/Azit/Views/Upload/EmojiView.swift
+++ Azit/Azit/Views/Upload/EmojiView.swift
@@ -6,19 +6,17 @@
 //
 
 import SwiftUI
-import EmojiPicker
+//import EmojiPicker
 
 struct EmojiView : View {
     @Binding var message: String
     @Binding var selectedEmoji: Emoji?
     @State var isShowingsheet: Bool = false
-
     
     var body : some View{
         VStack {
-            
             NavigationStack {
-                EmojiPickerView(selectedEmoji: $selectedEmoji, selectedColor: .accent)
+                EmojiPickerView(selectedEmoji: $selectedEmoji, selectedColor: Color.accent)
                     .background(Color.subColor4)
                     .toolbar {
                         ToolbarItem(placement: .navigationBarLeading) {
@@ -45,54 +43,75 @@ struct EmojiView : View {
                             }
                         }
                     }
-                    
-                    }.frame(width: UIScreen.main.bounds.width*0.9, height: UIScreen.main.bounds.height * 1.1 / 3)
-                    .padding(.bottom)
                 
-                // 메시지 입력
-                TextField("상태 메시지를 입력하세요.", text: $message)
+            }.frame(width: UIScreen.main.bounds.width*0.9, height: UIScreen.main.bounds.height * 1.1 / 3)
+                .padding(.bottom)
+            
+            // 메시지 입력
+            TextField("상태 메시지를 입력하세요.", text: $message)
+                .padding(.leading, 10)
+                .frame(width: 340, height: 40)
+                .background(
+                    RoundedRectangle(cornerRadius: 6)
+                        .stroke(Color.subColor1, lineWidth: 0.5)
+                        .background(Color.white.clipShape(RoundedRectangle(cornerRadius: 10)))
+                )
+                .padding(.bottom)
+            
+            // camera button
+            NavigationLink(destination: CameraView()) {
+                RoundedRectangle(cornerSize: CGSize(width: 12.0, height: 12.0))
+                    .background(RoundedRectangle(cornerSize: CGSize(width: 12.0, height: 12.0))
+                        .fill(Color.accentColor))
                     .frame(width: 340, height: 40)
-                    .background(.white)
-                    .padding(.bottom)
-                
-                // camera button
-                NavigationLink(destination: CameraView()) {
-                    RoundedRectangle(cornerSize: CGSize(width: 12.0, height: 12.0))
-                        .background(RoundedRectangle(cornerSize: CGSize(width: 12.0, height: 12.0))
-                            .fill(Color.accentColor))
-                        .frame(width: 340, height: 40)
-                        .overlay(Image(systemName: "camera.fill")
-                            .padding()
-                            .foregroundColor(Color.white)
-                        )
-                }
-                .padding(.bottom, 20)
+                    .overlay(Image(systemName: "camera.fill")
+                        .padding()
+                        .foregroundColor(Color.white)
+                    )
             }
-            .frame(width: 365, height: 500) // 팝업창 크기
-            .background(
-                RoundedRectangle(cornerRadius: 15)
-                    .fill(Color.subColor4)
-                    .stroke(Color.accentColor, lineWidth: 0.5)
-                    .shadow(radius: 10)
-
-            )
-            .padding()
-            .sheet(isPresented: $isShowingsheet) {
-                PublishScopeView()
-                    .presentationDetents([.medium])
-            }
-        }
-    
-//    func getEmojiList()->[[Int]] {
-//        var emojis : [[Int]] = []
-//        for i in stride(from: 0x1F601, to: 0x1F64F, by: 4){
-//            var temp : [Int] = []
-//            for j in i...i+3{
-//                temp.append(j)
+            .padding(.bottom, 20)
+            
+            // 공유 버튼
+//            Button (action:{
+//                
+//            }) {
+//                RoundedRectangle(cornerSize: CGSize(width: 12.0, height: 12.0))
+//                    .background(RoundedRectangle(cornerSize: CGSize(width: 12.0, height: 12.0))
+//                        .fill(Color.white))
+//                    .stroke(Color.accentColor, lineWidth: 0.5)
+//                    .frame(width: 340, height: 40)
+//                    .overlay(Text("Share")
+//                        .padding()
+//                        .foregroundColor(Color.accentColor)
+//                    )
+//                
 //            }
-//            emojis.append(temp)
-//        }
-//        return emojis
-//    }
+        }
+        .frame(width: 365, height: 500) // 팝업창 크기
+        .background(
+            RoundedRectangle(cornerRadius: 15)
+                .fill(Color.subColor4)
+                .stroke(Color.accentColor, lineWidth: 0.5)
+                .shadow(radius: 10)
+            
+        )
+        .padding()
+        .sheet(isPresented: $isShowingsheet) {
+            PublishScopeView()
+                .presentationDetents([.medium])
+        }
+    }
+    
+    //    func getEmojiList()->[[Int]] {
+    //        var emojis : [[Int]] = []
+    //        for i in stride(from: 0x1F601, to: 0x1F64F, by: 4){
+    //            var temp : [Int] = []
+    //            for j in i...i+3{
+    //                temp.append(j)
+    //            }
+    //            emojis.append(temp)
+    //        }
+    //        return emojis
+    //    }
 }
 
