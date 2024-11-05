ff32f0099b0bca1dafdda034cfe52d670d46394f
Bbell <428bbell@gmail.com>
2024-11-05 22:21:30 +0900

    [Design]:MyPageView

---
+++ Azit/Azit/Views/MyPage/MyPageView.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageView.swift
+//  Azit
+//
+//  Created by 김종혁 on 11/5/24.
+//
+
+import SwiftUI
+
+struct MyPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyPageView()
+}
