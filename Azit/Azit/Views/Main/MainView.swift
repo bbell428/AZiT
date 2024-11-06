0ac8ad3a99efd9b72de9925ca3740b1684abe3be
ParkJunYoung <j77777y@naver.com>
2024-11-06 21:38:13 +0900

    [Design]: 메인화면 UI 수정
    1. 지도 상단에 gradient 적용
    2. 버튼 위치 변경 (메시지 우측 상단으로 변경)
    3. 우측 상단 버튼 모음 백그라운드 적용

--- Azit/Azit/Views/Main/MainView.swift
+++ Azit/Azit/Views/Main/MainView.swift
@@ -59,62 +59,74 @@ struct MainTopView: View {
     
     var body: some View {
         VStack {
-            HStack() {
-                Text("AZiT")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                    .bold()
-                    .padding()
-                
-                Spacer()
-                
-                HStack(spacing: 20) {
-                    Button {
-                        // 게시글 리로드
-                    } label: {
-                        Image(systemName: "arrow.clockwise")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 25)
-                    }
-                    .disabled(isModalPresented ? true : false)
-                    
-                    Button {
-                        // 앨범 리스트
-                    } label: {
-                        Image(systemName: "photo.stack")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 30)
-                    }
-                    
-                    NavigationLink {
-                        MyPageView()
-                    } label: {
-                        Image(systemName: "person.fill")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 30)
-                    }
-                    
+            ZStack {
+                if isMainExposed == false {
+                    Rectangle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [Color.clear, Color.white]),
+                                             startPoint: .bottom,
+                                             endPoint: .top))
                 }
-                .padding()
+                
+                HStack() {
+                    Text("AZiT")
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                        .bold()
+                        .padding()
+                    
+                    Spacer()
+                    
+                    HStack(spacing: 20) {
+//                        Button {
+//                            // 게시글 리로드
+//                        } label: {
+//                            Image(systemName: "arrow.clockwise")
+//                                .resizable()
+//                                .aspectRatio(contentMode: .fit)
+//                                .frame(width: 25)
+//                        }
+//                        .disabled(isModalPresented ? true : false)
+                        
+                        NavigationLink {
+                            MessageView()
+                        } label: {
+                            Image(systemName: "ellipsis.message.fill")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 30)
+                        }
+                        
+                        Button {
+                            // 앨범 리스트
+                        } label: {
+                            Image(systemName: "photo.stack")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 30)
+                        }
+                        
+                        NavigationLink {
+                            MyPageView()
+                        } label: {
+                            Image(systemName: "person.fill")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 30)
+                        }
+                    }
+                    .frame(width: 150, height: 50)
+                    .background(Color.gray.opacity(0.1))
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                    .padding()
+                }
+                .foregroundStyle(.accent)
             }
-            .foregroundStyle(.accent)
+            .frame(maxHeight: (screenBounds?.height ?? 0) * 0.25)
+            .ignoresSafeArea()
             
             Spacer()
             
             HStack{
-                NavigationLink {
-                    MessageView()
-                } label: {
-                    Image(systemName: "ellipsis.message.fill")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 40, height: 40)
-                }
-                .padding()
-                
                 Spacer()
                 
                 Button {
@@ -123,10 +135,18 @@ struct MainTopView: View {
                     Image(systemName: isMainExposed ? "map" : "house")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(width: 40)
+                        .frame(width: 30)
                 }
-                .padding()
+                .frame(width: 60, height: 60)
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(.accent, lineWidth: 2) // 원하는 색상과 선 두께로 설정
+                )
             }
+            .padding()
+            
         }
         .frame(maxWidth: screenBounds?.width, maxHeight: screenBounds?.height)
     }
