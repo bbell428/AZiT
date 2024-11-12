ebc55ca2a08e6cd1835c5a92dbd3fe9a7516e796
First Snow <show2633@naver.com>
2024-11-12 13:14:34 +0900

    [Design]: story에 image가 있을 때 content 분기 추가

--- Azit/Azit/Views/Main/StoryContentsView.swift
+++ Azit/Azit/Views/Main/StoryContentsView.swift
@@ -13,17 +13,24 @@ struct StoryContentsView: View {
     var body: some View {
         // story에 image가 있을 때
         if story?.image ?? "" != "" {
-            HStack() {
-                Text(story?.content ?? "")
-                
-                Spacer()
+            // story에 content가 있을 때
+            if story?.content ?? "" != "" {
+                HStack() {
+                    Text(story?.content ?? "")
+                    
+                    Spacer()
+                }
             }
-            // UIImage로 storage에 있는 image 불러오기
-            Image("")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .clipShape(RoundedRectangle(cornerRadius: 8))
-                .onTapGesture { }
+            
+            VStack {
+                // UIImage로 storage에 있는 image 불러오기
+                Image(.realToBed)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                    .onTapGesture { }
+            }
+            
         // story에 image가 없을 때
         } else {
             // story에 emoji가 있을 때
