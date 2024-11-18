5e2a8d4969d5c6545e00d487dff46167aa0b702d
Jisoo <voidmule.xx@gmail.com>
2024-11-19 03:12:03 +0900

    [Feat]: PublicScopeView에 공개 범위 설정 기능 추가

--- Azit/Azit/Views/Upload/EmojiUpload/PublishScopeView.swift
+++ Azit/Azit/Views/Upload/EmojiUpload/PublishScopeView.swift
@@ -13,90 +13,196 @@ struct PublishScopeView: View {
     @EnvironmentObject var authManager: AuthManager
     @EnvironmentObject var userInfoStore: UserInfoStore
     
-    @State var isSelected: [String : Bool] = [:]    
-    @State var AllSelected: Bool = true
+    @State var isAllSelected: Bool = true
+    @State var isSelected: [String : Bool] = [:]
+    @State private var scale: CGFloat = 0.1
+//    @State var friends: [UserInfo] = []
+//    @State var friendID: String = ""
+
     
     var body: some View {
-        List {
-            Button (action: {
-                AllSelected = true
-                if AllSelected {
-                    for friendID in userInfoStore.userInfo?.friends ?? [] {
-                        isSelected[friendID] = false
-                        if let friendNickname = userInfoStore.friendInfo[friendID]?.nickname {
-                            if !storyDraft.publishedTargets.contains(friendNickname) {
-                                storyDraft.publishedTargets.append(friendNickname)
+        VStack() {
+            Text("공개 범위")
+                .padding([.top, .bottom], 30)
+            Spacer()
+            
+            
+            if userInfoStore.userInfo?.friends.count == 0 {
+                Text("친구를 초대해보세요!")
+            } else {
+                Divider()
+                
+                ScrollView(showsIndicators: false) {
+                    // ALL에게 공개
+                    Button (action: {
+                        isAllSelected = true
+                        if isAllSelected {
+                            // 모든 친구 선택
+                            if let friendsID = userInfoStore.userInfo?.friends {
+                                for friendID in friendsID {
+                                    isSelected[friendID] = false
+                                    if let friendNickname = userInfoStore.friendInfo[friendID]?.nickname {
+                                        if !storyDraft.publishedTargets.contains(friendNickname) {
+                                            storyDraft.publishedTargets.append(friendNickname)
+                                        }
+                                    }
+                                }
                             }
-                        } else {
-                            // 모든 친구 선택 해제
-                            for friendID in userInfoStore.userInfo?.friends ?? [] {
-                                isSelected[friendID] = false
+                        }
+                    }
+                    ) {
+                        HStack {
+                            ZStack {
+                                Circle()
+                                    .frame(width: 40, height: 40)
+                                    .foregroundStyle(isAllSelected ? .accent.opacity(0.5) : .subColor4)
+                                Image(systemName: "person.fill")
+                                    .foregroundStyle(.accent)
                             }
-                            storyDraft.publishedTargets.removeAll()
+                            .padding()
+                            Text("ALL")
+                                .font(.title3)
+                                .foregroundStyle(.accent)
+                            Spacer()
+                            
+                            if isAllSelected {
+                                Image(systemName: "checkmark")
+                                    .foregroundStyle(.accent)
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                    
+                    Divider()
+                    // MARK: - 친구 리스트
+                    if let friendsID = userInfoStore.userInfo?.friends {
+                        ForEach(friendsID, id: \.self) { friendID in
+                            Button(action: {
+                                isAllSelected = false
+//                                isSelected[friendID]?.toggle()
+                                if isSelected[friendID] ?? true {
+                                    isSelected[friendID] = false
+                                } else {
+                                    isSelected[friendID] = true
+                                }
+                                
+                                if let isSelected = isSelected[friendID] {
+                                    storyDraft.publishedTargets.append(userInfoStore.friendInfo[friendID]?.id ?? "")
+                                } else {
+                                    storyDraft.publishedTargets.removeAll { $0 == (userInfoStore.friendInfo[friendID]?.id ?? "") }
+                                }
+                                
+                                
+                            }) {
+                                HStack {
+                                    if let isSelected = isSelected[friendID] {
+                                        ZStack {
+                                            Circle()
+                                                .frame(width: 40, height: 40)
+//                                                .foregroundStyle(.subColor4)
+                                                .foregroundStyle(isSelected ? .accent.opacity(0.5) : .subColor4)
+                                            Text(userInfoStore.friendInfo[friendID]?.profileImageName ?? "")
+                                                .font(.title3)
+                                        }
+                                        .padding()
+                                        Text(userInfoStore.friendInfo[friendID]?.nickname ?? "")
+                                            .font(.title3)
+                                        //                                    .foregroundStyle(isSelected[friendID] ?? nil ? Color.accentColor : Color.black)
+                                        Spacer()
+                                        if isSelected {
+                                            Image(systemName: "checkmark")
+                                                .foregroundStyle(.accent)
+                                        }
+                                    }
+                                }
+                            }
+                            .padding(.horizontal, 10)
+                            Divider()
+                        }
+                    } else {
+                        VStack {
+                            Image(systemName: "person.fill.badge.plus")
+                            Text("친구를 초대해보세요!")
+                        }
+                        .font(.headline)
+                        .foregroundStyle(Color.accentColor)
+                    }
+                }
+                .padding([.leading, .trailing])
+                .presentationDetents([.fraction(0.5)])
+                .presentationDragIndicator(.visible)
+                .onAppear {
+                    Task {
+                        await userInfoStore.loadUserInfo(userID: authManager.userID)
+                        userInfoStore.loadFriendsInfo(friendsIDs: userInfoStore.userInfo?.friends ?? [])
+                    }
+                }
+                .onAppear {
+                    // `isSelected` 초기화
+                    if let friends = userInfoStore.userInfo?.friends {
+                        for friend in friends {
+                            if isSelected[friend] == nil {
+                                isSelected[friend] = false
+                            }
                         }
                     }
                 }
-            }) {
-                HStack {
-                    ZStack {
-                        Circle()
-                            .frame(width: 50, height: 50)
-                            .foregroundStyle(.subColor4)
-                        Image(systemName: "person")
-                            .foregroundStyle(.accent)
-                    }
-                    Text("ALL")
-                        .font(.title2)
-                        .foregroundStyle(.accent)
-                    Spacer()
-                    if AllSelected {
-                        Image(systemName: "checkmark")
-                            .foregroundStyle(.accent)
-                    }
-                }
-            }
-            .padding(10)
-            
-            // 친구 리스트
-//            if let friendsID = userInfoStore.userInfo?.friends {
-//                ForEach(friendsID, id: \.self) { friendID in
-//                    Button(action: {
-//                        isSelected[friendID]?.toggle()
-//                        if isSelected[friendID] ?? true {
-//                            storyDraft.publishedTargets.append(userInfoStore.friendInfo[friendID]?.nickname ?? "")
-//                        } else {
-//                            storyDraft.publishedTargets.remove(at: userInfoStore.friendInfo[friendID]?.nickname ?? "")
-//                        }
-//                        
-//                    }) {
-//                        HStack {
-//                            ZStack {
-//                                Circle()
-//                                    .frame(width: 50, height: 50)
-//                                    .foregroundStyle(.secondary)
-//                                Text(userInfoStore.friendInfo[friendID]?.profileImageName ?? "")
-//                                    .font(.title2)
-//                            }
-//                            Text(userInfoStore.friendInfo[friendID]?.nickname ?? "")
-//                                .font(.title2)
-//                            Spacer()
-//                            if isSelected[friendID] {
-//                                Image(systemName: "checkmark")
-//                                    .foregroundStyle(.accent)
-//                            }
-//                        }
-//                    }
-//                    .padding(10)
-//                }
-//            } else {
-//                Text("No friends available")
+                
+                //            VStack(alignment: .center) {
+                //MARK: 친구 목록
+                //                ForEach(Array(friends.prefix(3)), id: \.id) { friend in
+                //                    HStack {
+                //                        ZStack {
+                //                            Circle()
+                //                                .fill(Color.subColor4)
+                //                                .frame(width: 45, height: 45)
+                //                            Text(friend.profileImageName)
+                //                                .font(.system(size: 30))
+                //                                .bold()
+                //                        }
+                //                        Text(friend.nickname)
+                //                            .fontWeight(.light)
+                //                            .foregroundStyle(Color.gray)
+                //
+                //                        Spacer()
+                //
+                //                        Button {
+                //                            isEditFriend.toggle()
+                //                            friendID = friend.id
+                //                        } label: {
+                //                            Image(systemName: "line.horizontal.3")
+                //                                .foregroundColor(.gray)
+                //                                .padding(.trailing, 20)
+                //                                .font(.title3)
+                //                        }
+                //                        .overlay {
+                //                            if isEditFriend && friendID == friend.id {
+                //                                EditFriend(friendID: friendID, isEditFriend: $isEditFriend)
+                //                                    .scaleEffect(scale)
+                //                                    .onAppear {
+                //                                        withAnimation(.easeInOut(duration: 0.3)) {
+                //                                            scale = 1.0
+                //                                        }
+                //                                    }
+                //                                    .onDisappear {
+                //                                        withAnimation(.easeInOut(duration: 0.3)) {
+                //                                            scale = 0.1
+                //                                        }
+                //                                    }
+                //                                    .padding(.leading, -40)
+                //                            }
+                //                        }
+                //                    }
+                //                    .padding(.vertical, 1)
+                //
+                //                    Divider()
+                //                }
+                //            }
 //            }
+//            .padding(.horizontal)
+//            .padding(.bottom, 50)
         }
-        .onAppear {
-            Task {
-                await userInfoStore.loadUserInfo(userID: authManager.userID)
-                userInfoStore.loadFriendsInfo(friendsIDs: userInfoStore.userInfo?.friends ?? [])
-            }
+
         }
     }
 }
