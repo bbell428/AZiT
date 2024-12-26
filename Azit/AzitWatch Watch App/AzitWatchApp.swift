e3fa156cb4a4d8aa7cc1be1de94feb65dc66438e
First Snow <show2633@naver.com>
2024-12-27 00:26:25 +0900

    [Add]: Base Watch App 추가

---
+++ Azit/AzitWatch Watch App/AzitWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AzitWatchApp.swift
+//  AzitWatch Watch App
+//
+//  Created by Hyunwoo Shin on 12/27/24.
+//
+
+import SwiftUI
+
+@main
+struct AzitWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
