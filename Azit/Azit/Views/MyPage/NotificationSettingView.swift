6e889dc24cd97ff6b4cc486237c6e9bc726d09a9
김종혁 <428bbell@gmail.com>
2025-02-07 01:38:56 +0900

    [Design]: NotificationSettingView - 스위치 UI 디자인 및 alert

--- Azit/Azit/Views/MyPage/NotificationSettingView.swift
+++ Azit/Azit/Views/MyPage/NotificationSettingView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct NotificationSettingView: View {
     @Environment(\.dismiss) var dismiss
-    @State private var isNotificationsEnabled = true // 버튼 클릭 시
+    @State private var isNotificationsEnabled = false // 버튼 클릭 시
+    @State private var isAlert = false
     
     var body: some View {
         VStack(alignment: .center) {
@@ -34,17 +35,38 @@ struct NotificationSettingView: View {
             .frame(height: 70)
             .background(Color.white)
             
+            // 알림 설정 스위치
             VStack {
-                Toggle(isOn: $isNotificationsEnabled) {
-                    Text("전체 알림")
+                Toggle(isOn: Binding(
+                    get: { isNotificationsEnabled },
+                    set: { newValue in
+                        if newValue {
+                            isAlert = true  // ON으로 변경 시 Alert 띄우기
+                        } else {
+                            isNotificationsEnabled = false
+                        }
+                    }
+                )) {
+                    Text("전체 알림 메시지 해제")
                         .font(.headline)
                 }
-                .toggleStyle(SwitchToggleStyle(tint: .green))  // 초록색 ON (true)
+                .toggleStyle(SwitchToggleStyle(tint: .green))
                 .padding()
             }
+            
             Spacer()
         }
         .frame(width: 370)
         .navigationBarBackButtonHidden(true)
+        .alert("메시지 알림을 끄시겠습니까?", isPresented: $isAlert) {
+            Button("아니요", role: .cancel) {
+                isNotificationsEnabled = false
+            }
+            Button("예", role: .destructive) {
+                isNotificationsEnabled = true
+            }
+        } message: {
+            Text("알림을 끄면 중요한 메시지를 받을 수 없습니다.")
+        }
     }
 }
